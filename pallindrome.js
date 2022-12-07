011e916be4ebcbeb3442b3565532ab118de526b9
Bindhu Madavali <bindhumadavali2000@gmail.com>
2022-12-08 00:35:43 +0530

    poo bathuko

--- pallindrome.js
+++ pallindrome.js
@@ -1,4 +1,12 @@
 let str="madam" 
-for(i=str.length-1;i>=0;i++) {
+let bag = "";
+for(i=str.length-1;i>=0;i--) {
+    bag+str[i];
     
+}
+if(str==bag){
+    console.log("True");
+}
+else{
+    console.log("number is not palindrome");
 }
--- prime.js
+++ prime.js
@@ -4,4 +4,11 @@ for(i=1;i<num;i++) {
     if(num%i==0) {
        count++ 
     }
-}
+}    
+    if(count==2){
+        console.log("Is a prime");
+
+    }
+    else{
+        console.log("is not a prime");
+    }
